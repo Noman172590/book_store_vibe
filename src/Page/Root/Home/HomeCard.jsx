9b189af43932f0f78180adfed8a0002d779d679b
Noman172590 <nomanjahan866615@gmail.com>
2026-09-17 20:07:37 +0600

    card update

--- src/Page/Root/Home/HomeCard.jsx
+++ src/Page/Root/Home/HomeCard.jsx
@@ -1,45 +1,33 @@
 import { use } from "react";
+import Button from "../../../Components/Button/Button";
 
-const HomeCard = ({data}) => {
-    const newData=use(data)
-    console.log(newData)
+const HomeCard = ({ data }) => {
+  const newData = use(data);
+  console.log(newData);
   return (
     <section>
-      <p className="font-playfair font-bold text-[40px] text-center mb-9">Books</p>
-      <div className="shadow border">
-        <img src="" alt="" />
+      <p className="font-playfair font-bold text-[40px] text-center mb-9">
+        Books
+      </p>
+      <div className="grid grid-cols-3 gap-6">
+        {newData.map((item) => (
+          <div className="shadow border">
+            <img className="h-41.5 mx-auto" src={item.image} alt="" />
 
-        <div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat
-            doloribus deserunt fugiat? Possimus harum, repudiandae at debitis
-            explicabo cupiditate, amet assumenda tempore quos, voluptates
-            ducimus iusto rem facilis commodi deserunt.
-          </p>
-          <p>
-            Ad esse dolor deserunt excepturi, et provident cum dignissimos, sunt
-            placeat itaque quibusdam, dicta cupiditate labore. Necessitatibus
-            fugit sequi odio dolore dignissimos nesciunt, iste doloremque nemo
-            quidem illum unde quae?
-          </p>
-        </div>
+            <div>
+              <Button title="Young Adult"/>
+              <Button title="Identity"/>
+              
+            </div>
 
-        <p>l</p>
-        <p>l</p>
-        <div>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-            nihil maxime eos at aliquid tempora doloremque illo ipsa minus,
-            rerum animi placeat tempore voluptate a suscipit nisi asperiores,
-            eaque aperiam.
-          </p>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
-            nihil maxime eos at aliquid tempora doloremque illo ipsa minus,
-            rerum animi placeat tempore voluptate a suscipit nisi asperiores,
-            eaque aperiam.
-          </p>
-        </div>
+            <p>{item.bookName}</p>
+            <p>By: {item.author}</p>
+            <div>
+              <p>{item.tags[0]}</p>
+              <p>{item.tags[1]}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
